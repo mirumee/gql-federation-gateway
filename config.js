b76ec6176c662000e465a09f45e117a1c2c1c30d
Jakub Draganek <jakub.draganek@gmail.com>
2019-10-21 15:16:33 +0200

    Parse service list from env using name:url name2:url2 format

--- config.js
+++ config.js
@@ -1,5 +1,13 @@
 const dotenv = require('dotenv');
 dotenv.config();
 module.exports = {
-  endpoints: JSON.parse(process.env.ENDPOINTS)
+  endpoints: parseEndpoints(process.env.ENDPOINTS)
 };
+
+function parseEndpoints(conf) {
+  const endpoints = conf.split(' ')
+  return endpoints.map((endpoint) => {
+    const delimiter = endpoint.indexOf(':');
+    return { name: endpoint.substr(0, delimiter), "url": endpoint.substr(delimiter + 1) }
+  })
+}
